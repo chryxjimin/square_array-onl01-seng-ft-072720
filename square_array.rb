9ee0ad869381f54ce551271f1ceb09216e3bd01d
Christie Yoon <cleex1999@gmail.com>
2020-07-31 00:16:23 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,12 @@
 def square_array(numbers)
   numbers = [1,2,3]
   numbers.each do |nums|
-    puts "nums**2"
+  nums**2
 end
 
 #print square_array(numbers)
+#def square_array(array)
+    #array.each do |i|
+    #  i ** 2
+    #end
+#end
